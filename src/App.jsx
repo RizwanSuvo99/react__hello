a501fd3e42ad550457d0d1c515fe7b78e07e3729
Rizwan Suvo <rizwanshuvo@gmail.com>
2024-04-01 10:29:26 +0600

    Initial Commit Done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+      <main className="flex flex-col justify-center h-screen items-center ">
+        <div className="p-5 bg-yellow-400 max-w-[50rem]"  >
+          <h1 className="text-4xl">Hello World</h1>
+          <p className="text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea non quae minus tenetur fugit quod ad et voluptatibus voluptatem est voluptate nemo sequi unde, molestiae assumenda praesentium, repellat laudantium nihil ratione maiores! Laboriosam eum nihil, suscipit labore fuga, aut dolor, veritatis soluta obcaecati officiis repellat asperiores ea perferendis sed ut.</p>
+        </div>
+     </main>
+    </>
+  )
+}
+
+export default App
